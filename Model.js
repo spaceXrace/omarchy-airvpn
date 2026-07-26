f698a6ec67de7dcdd5c8987c89c7ba8ba69e2340
spaceXrace <92670772+spaceXrace@users.noreply.github.com>
2026-07-26 13:18:35 +0200

    Use AirVPN max bandwidth for filters

--- Model.js
+++ Model.js
@@ -33,6 +33,8 @@ function serverSubtitle(row) {
   if (row.country) parts.push(row.country)
   var bw = formatBandwidth(row.bandwidth)
   if (bw) parts.push(bw)
+  var usage = formatBandwidth(row.usage)
+  if (usage) parts.push(usage + " used")
   if (row.users) parts.push(row.users + " users")
   var load = formatLoad(row.load)
   if (load) parts.push(load + " load")
